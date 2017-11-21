f81eedea79bca9561106f6082a0c36659f06e2a1
appoliana <polinapikkio@gmail.com>
2017-11-21 20:24:20 +0300

    My second homework

---
+++ 2homework/2.4/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: polinapicchio
+ *
+ * Created on 7 октября 2017 г., 16:11
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
