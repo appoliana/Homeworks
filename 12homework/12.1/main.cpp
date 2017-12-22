3e4ce50c8f93071da0496691339901ef78fc50e0
appoliana <polinapikkio@gmail.com>
2017-12-22 15:48:11 +0300

    WORKgit add .

--- 12homework/12.1/main.cpp
+++ 12homework/12.1/main.cpp
@@ -6,84 +6,54 @@
 
 using namespace std;
 
-/*void countElements(int &length) 
-{
-    FILE * File = fopen("text.txt", "r");
-    int p = 0;
-    while (!feof(File)) {
-	fscanf(File, "%d", &p);
-	++length;
-    }
-    fclose(File);
-}
-*/
-
-string algorithmKMP (string s, string p)
+void algorithmKMP (string s, string p)
 {
     int i, j, N, M;
     N = s.length();
     M = p.length();
     string uniqElement = "@";
     string sum = p + uniqElement + s;
-    // cout << sum;
-    string result = "0";
+    cout << sum << endl;
+    int *result = new int[s.length() + p.length()];
     int *value = new int[N + M + 1];
     value[0] = 0;
-    int count = 0;
-    for (i = 1; i < N + M + 1; i++) {
+    for (i = 1; i < N + M + 1; i++) 
+    {
         j = 0;
-        if (sum[i] != sum[j]) {
+        if (sum[i] != sum[j]) 
+        {
             value[i] = 0;
             continue;
         }
-        else {
-            while (sum[i] == sum[j]) {
+        else 
+        {
+            while (sum[i] == sum[j]) 
+            {
                 value[i] = j + 1;
                 j++;
                 i++;
             }
         } 
+        i--; 
+    }
+ 
+    for (int i = 0; i < N + M + 1; i++) 
+    {
         if (value[i] == M){
-            result[count] = i - M + 1;
-            count ++;
+            cout << i - 3 << " - вхождение" << endl;
         }
     }
+    
     delete[] value;
-    return result;
 }
 
 int main(int argc, char** argv) {
-    /*int lengthS = 0;
-    countElements(lengthS);
-    int *s = new int[lengthS];
-    cout << lengthS << endl;
-    FILE * File = fopen("text.txt", "r");
-    for (int i = 0; i < length; ++i) {
-	fscanf(File, "%d", &s[i]);
-    }
-    fclose(File);
-    */
-    
-    /*cout << "Введите строку: " << endl;
-    char *s = new char[10];
-    cin >> s;
-    int lengthS = strlen(s);
-    
-    
-    cout << "Введите образ: " << endl;
-    char *p = new char[10];
-    cin >> p;
-    int lengthP = strlen(p) + strlen(s) + 1;
-    */
     string s = "ababba";
     cout << "Исходная строка s: " << s << endl;
     string p = "ab";
     cout << "Образец p: " << p << endl;
-    string result = "0";
   
-    result = algorithmKMP(s, p);
-    
-    cout << "Номера вхождений образа p в строку s: " << result << endl;
+    algorithmKMP(s, p);
    
     return 0;
 }
