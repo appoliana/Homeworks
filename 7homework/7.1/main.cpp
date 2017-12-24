e365dda3c8a150a0f950fff90ac7d917cfb067ef
appoliana <polinapikkio@gmail.com>
2017-12-24 17:47:10 +0300

    7.1 done

--- 7homework/7.1/main.cpp
+++ 7homework/7.1/main.cpp
@@ -3,26 +3,39 @@
 
 using namespace std;
 
+void menu() {
+    cout << "Введите 1 для вывода элементов в порядке возрастания;" << endl;
+    cout << "Введите 2 для вывода элементов в порядке убывания;" << endl;
+    cout << "Введите 0 для завершения работы программы;" << endl;
+}
+
 int main(int argc, char** argv) {
     BinarySearchTree *tree = createTree();
+    
     add(tree, 4);
     add(tree, 8);
     add(tree, 3);
     add(tree, 1);
   
-    int choose = 0;
-    cout << "Введите 1 для вывода элементов в порядке возрастания, 2 - в порядке убывания: " << endl;
-    cin >> choose;
+    int choose = 3;
+    menu();
     while (choose != 0) {
+        cin >> choose;
         switch (choose) {
+            case 0:
+            {
+                return 2;
+            }
             case 1:
             {
                 printMin(tree);
+                cout << endl;
                 break;
             }
             case 2:
             {
                 printMax(tree);
+                cout << endl;
                 break;
             }
             default:
